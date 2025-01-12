4ed81cd3693b72355f869acceafb6442ac4b3e54
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-12 09:54:20 +0300

    Mobile navbar regulations

--- src/assets/components/layout/Navbar/Navbar.tsx
+++ src/assets/components/layout/Navbar/Navbar.tsx
@@ -10,16 +10,19 @@ import {
 import "./_Navbar.scss";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
-
+import { IoSearchOutline } from "react-icons/io5";
 function NavbarComp() {
   return (
     <>
       <Navbar className="bg-body-tertiary px-0">
         <Container>
-          <GiHamburgerMenu className="fs-1 hamburger-menu" />
-          <NavbarBrand href="#" className="bg-transparent">
-            <img src="/src/assets/images/Logo1.png" className="Navbar-Logo" />
-          </NavbarBrand>
+          <div className="d-flex align-items-center justify-content-between w-75">
+            <GiHamburgerMenu className="fs-1 hamburger-menu m-1" />
+
+            <NavbarBrand href="#" className="bg-transparent">
+              <img src="/src/assets/images/Logo1.png" className="Navbar-Logo" />
+            </NavbarBrand>
+          </div>
 
           <NavbarToggle aria-controls="navbarScroll" className="d-lg-none" />
           <NavbarCollapse className="d-flex justify-content-end">
@@ -47,6 +50,15 @@ function NavbarComp() {
           </NavbarCollapse>
         </Container>
       </Navbar>
+      <Form className="d-flex rounded-4 mx-1 mb-2 align-items-center bg-body-secondary">
+        <IoSearchOutline className="fs-4" />
+        <Form.Control
+          type="search"
+          placeholder="Aradıgınız ürünü yazınız...."
+          className="border-0 rounded-4 bg-body-secondary custom-form-control"
+          aria-label="Search"
+        />
+      </Form>
     </>
   );
 }
