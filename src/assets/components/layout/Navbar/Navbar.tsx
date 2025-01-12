7dd0e62ea1b21c4034ac85a577cc620f357b6f49
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-12 09:38:07 +0300

    mobil navbarda düzenlemeler yapıldı

--- src/assets/components/layout/Navbar/Navbar.tsx
+++ src/assets/components/layout/Navbar/Navbar.tsx
@@ -1,39 +1,54 @@
-import { Button, Container, NavbarBrand, NavbarCollapse, Navbar, Form, NavbarToggle, NavDropdown } from 'react-bootstrap';
-import './Navbar.scss'
+import {
+  Button,
+  Container,
+  NavbarBrand,
+  NavbarCollapse,
+  Navbar,
+  Form,
+  NavbarToggle,
+} from "react-bootstrap";
+import "./_Navbar.scss";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function NavbarComp() {
+  return (
+    <>
+      <Navbar className="bg-body-tertiary px-0">
+        <Container>
+          <GiHamburgerMenu className="fs-1 hamburger-menu" />
+          <NavbarBrand href="#" className="bg-transparent">
+            <img src="/src/assets/images/Logo1.png" className="Navbar-Logo" />
+          </NavbarBrand>
 
-    return <>
-        <Navbar className="bg-body-tertiary px-5">
-            <Container>
-                <NavbarBrand href="#">
-                    <img src="/src/assets/images/Logo1.png" className='Navbar-Logo' />
-                </NavbarBrand>
-                <NavbarToggle aria-controls="navbarScroll" />
-                <NavbarCollapse className='d-flex justify-content-center'>
-                    <Form className="d-flex me-3">
-                        <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-                        <Button variant="secondary">Search</Button>
-                    </Form>
-                    <div className="dropdown">
-                        <Button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Hesap
-                        </Button>
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </div>
-                </NavbarCollapse>
-            </Container>
-        </Navbar>
-
+          <NavbarToggle aria-controls="navbarScroll" className="d-lg-none" />
+          <NavbarCollapse className="d-flex justify-content-end">
+            <Form className="d-flex me-3" id="nav-search-form">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <Button variant="secondary">Search</Button>
+            </Form>
+            <div className="dropdown" id="nav-dropdown">
+              <Button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Hesap
+              </Button>
+            </div>
+            <NavbarBrand>
+              <AiOutlineShoppingCart className="fs-1 text-primary" />
+            </NavbarBrand>
+          </NavbarCollapse>
+        </Container>
+      </Navbar>
     </>
+  );
 }
 
 export default NavbarComp;
