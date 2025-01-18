ea9a7d27910eb9ebe98070b352f22a3a3d0f408c
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-18 21:41:04 +0300

    Adjustments were made to the orders design

--- src/routes/pages/Account/components/Orders.tsx
+++ src/routes/pages/Account/components/Orders.tsx
@@ -1,43 +1,88 @@
 import { Card, Button } from "react-bootstrap";
+import { PHOTO_URL } from "../../../../services/api/products";
 
 function Orders() {
   return (
     <div className="content-area">
       <h3 className="mb-4">Siparişlerim (4)</h3>
       <Card className="mb-3">
-        <Card.Body>
-          <h5>Teslim Edildi</h5>
-          <p>DEEP SLEEP</p>
-          <p>31 Mart 2023 Tarihinde Sipariş Verildi</p>
-          <p>427795 numaralı sipariş</p>
-          <Button variant="link">Detay Görüntüle</Button>
+        <Card.Body className="d-flex column-gap-5">
+          <div className="">
+            <img
+              src={PHOTO_URL + "/media/b12vitamini_.webp"}
+              className="img-fluid"
+              width={100}
+              height={100}
+              alt=""
+            />
+          </div>
+          <div className="">
+            <h5 className="text-success">Teslim Edildi</h5>
+            <p>MELATONİN - GÜNLÜK VİTAMİN PAKETİ - BROMELAIN</p>
+            <p>14 Aralık 2022 Tarihinde Sipariş Verildi</p>
+            <p>290405 numaralı sipariş</p>
+            <Button variant="link">Detay Görüntüle</Button>
+          </div>
         </Card.Body>
       </Card>
       <Card className="mb-3">
-        <Card.Body>
-          <h5>Teslim Edildi</h5>
-          <p>MELATONİN - GÜNLÜK VİTAMİN PAKETİ - BROMELAIN</p>
-          <p>14 Aralık 2022 Tarihinde Sipariş Verildi</p>
-          <p>290405 numaralı sipariş</p>
-          <Button variant="link">Detay Görüntüle</Button>
+        <Card.Body className="d-flex column-gap-5">
+          <div className="">
+            <img
+              src={PHOTO_URL + "/media/b12vitamini_.webp"}
+              className="img-fluid"
+              width={100}
+              height={100}
+              alt=""
+            />
+          </div>
+          <div className="">
+            <h5 className="text-success">Teslim Edildi</h5>
+            <p>MELATONİN - GÜNLÜK VİTAMİN PAKETİ - BROMELAIN</p>
+            <p>14 Aralık 2022 Tarihinde Sipariş Verildi</p>
+            <p>290405 numaralı sipariş</p>
+            <Button variant="link">Detay Görüntüle</Button>
+          </div>
         </Card.Body>
       </Card>
       <Card className="mb-3">
-        <Card.Body>
-          <h5>Teslim Edildi</h5>
-          <p>GAMER HACK - DETOX PAKETİ</p>
-          <p>19 Kasım 2022 Tarihinde Sipariş Verildi</p>
-          <p>255564 numaralı sipariş</p>
-          <Button variant="link">Detay Görüntüle</Button>
+        <Card.Body className="d-flex column-gap-5">
+          <div className="">
+            <img
+              src={PHOTO_URL + "/media/b12vitamini_.webp"}
+              className="img-fluid"
+              width={100}
+              height={100}
+              alt=""
+            />
+          </div>
+          <div className="">
+            <h5 className="text-success">Teslim Edildi</h5>
+            <p>MELATONİN - GÜNLÜK VİTAMİN PAKETİ - BROMELAIN</p>
+            <p>14 Aralık 2022 Tarihinde Sipariş Verildi</p>
+            <p>290405 numaralı sipariş</p>
+            <Button variant="link">Detay Görüntüle</Button>
+          </div>
         </Card.Body>
       </Card>
       <Card className="mb-3">
-        <Card.Body>
-          <h5>Teslim Edildi</h5>
-          <p>CREAM OF RICE</p>
-          <p>1 Ekim 2022 Tarihinde Sipariş Verildi</p>
-          <p>190462 numaralı sipariş</p>
-          <Button variant="link">Detay Görüntüle</Button>
+        <Card.Body className="d-flex column-gap-5">
+          <div className="">
+            <img
+              src={PHOTO_URL + "/media/b12vitamini_.webp"}
+              className="img-fluid"
+              width={100}
+              height={100}
+              alt=""
+            />
+          </div>
+          <div className="">
+            <h5 className="text-success">Teslim Edildi</h5>
+            <p>MELATONİN - GÜNLÜK VİTAMİN PAKETİ - BROMELAIN</p>
+            <p>14 Aralık 2022 Tarihinde Sipariş Verildi</p>
+            <p>290405 numaralı sipariş</p>
+            <Button variant="link">Detay Görüntüle</Button>
+          </div>
         </Card.Body>
       </Card>
     </div>
