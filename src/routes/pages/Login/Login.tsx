90f69e7bdedda5c7cd964ae8b38c2c04b4fc94ee
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-19 22:04:29 +0300

    Displaying member login and membership forms according to tabs

--- src/routes/pages/Login/Login.tsx
+++ src/routes/pages/Login/Login.tsx
@@ -12,31 +12,28 @@ function Login() {
                 return <Signup />;
             case "member_login":
                 return <MemberLogin />;
-
             default:
                 return <Signup />;
         }
     };
 
-
     return <>
-        <Container>
+        <Container className="my-5">
             <Row>
                 <Tabs
                     defaultActiveKey="profile"
                     id="uncontrolled-tab-example"
                     className="mb-3"
                 >
-                    <Tab eventKey="signup" title="Üye Ol">
+                    <Tab eventKey="signup" onClick={() => setActiveTab("signup")} title="Üye Ol">
                         {renderContent()}
                     </Tab>
-                    <Tab eventKey="member_login" title="Üye Girişi">
+                    <Tab eventKey="member_login" onClick={() => setActiveTab("member_login")} title="Üye Girişi">
                         {renderContent()}
                     </Tab>
                 </Tabs>
             </Row>
         </Container>
-
     </>
 }
 
