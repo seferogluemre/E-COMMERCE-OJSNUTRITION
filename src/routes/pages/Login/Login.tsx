7a1694f76e8efcae96d45b5315fd4c948355e22b
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-19 22:09:49 +0300

    Solving the tab problem on the login page

--- src/routes/pages/Login/Login.tsx
+++ src/routes/pages/Login/Login.tsx
@@ -4,37 +4,40 @@ import Signup from "./components/Signup";
 import MemberLogin from "./components/MemberLogin";
 
 function Login() {
-    const [activeTab, setActiveTab] = useState<string>("signup");
+  const [activeTab, setActiveTab] = useState<string>("signup");
 
-    const renderContent = () => {
-        switch (activeTab) {
-            case "signup":
-                return <Signup />;
-            case "member_login":
-                return <MemberLogin />;
-            default:
-                return <Signup />;
-        }
-    };
+  const renderContent = () => {
+    switch (activeTab) {
+      case "signup":
+        return <Signup />;
+      case "member_login":
+        return <MemberLogin />;
+      default:
+        return <Signup />;
+    }
+  };
 
-    return <>
-        <Container className="my-5">
-            <Row>
-                <Tabs
-                    defaultActiveKey="profile"
-                    id="uncontrolled-tab-example"
-                    className="mb-3"
-                >
-                    <Tab eventKey="signup" onClick={() => setActiveTab("signup")} title="Üye Ol">
-                        {renderContent()}
-                    </Tab>
-                    <Tab eventKey="member_login" onClick={() => setActiveTab("member_login")} title="Üye Girişi">
-                        {renderContent()}
-                    </Tab>
-                </Tabs>
-            </Row>
-        </Container>
+  return (
+    <>
+      <Container className="my-5">
+        <Row>
+          <Tabs
+            activeKey={activeTab}
+            onSelect={(k) => setActiveTab(k!)}
+            id="uncontrolled-tab-example"
+            className="mb-3"
+          >
+            <Tab eventKey="signup" title="Üye Ol">
+              {renderContent()}
+            </Tab>
+            <Tab eventKey="member_login" title="Üye Girişi">
+              {renderContent()}
+            </Tab>
+          </Tabs>
+        </Row>
+      </Container>
     </>
+  );
 }
 
 export default Login;
