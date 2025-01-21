7bb6588d25795ba276cefd7698285ccc59ea3a1c
seferogluemre <yunusemre5331@icloud.com>
2025-01-21 17:59:38 +0300

    Defining the types of the login form

--- src/routes/pages/Login/components/MemberLogin.tsx
+++ src/routes/pages/Login/components/MemberLogin.tsx
@@ -1,10 +1,58 @@
 import { Button, FormControl, FormGroup, FormLabel } from "react-bootstrap";
 import { Form } from "react-router-dom";
 
+interface ILoginFormInputs {
+  email: string;
+  password: string;
+}
+
+interface IApiData {
+  email: string;
+  password: string;
+  api_key: string;
+}
+
 function MemberLogin() {
+
+  const onSubmit = async (formData: IFormInputs) => {
+    try {
+      const dataForApi: IApiData = {
+        first_name: formData.firstName,
+        last_name: formData.lastName,
+        email: formData.email,
+        password: formData.password,
+        password2: formData.password,
+        api_key: "370718"
+      };
+
+      const response = await fetch("https://fe1111.projects.academy.onlyjs.com/api/v1/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataForApi),
+      });
+
+      const jsonResponse = await response.json();
+      
+      if (response.ok) {
+        // Başarılı kayıt işlemi
+        console.log("Kayıt başarılı:", jsonResponse);
+      } else {
+        // Hata durumu
+        console.error("Kayıt hatası:", jsonResponse);
+      }
+    } catch (error) {
+      console.error("Bir hata oluştu:", error);
+    }
+  };
+
+
+
+
   return (
     <>
-      <Form>
+      <Form onSubmit={onSubmit(onSubmit)}> 
         <FormGroup className="mb-3" controlId="loginEmail">
           <FormLabel>E-Posta</FormLabel>
           <FormControl
